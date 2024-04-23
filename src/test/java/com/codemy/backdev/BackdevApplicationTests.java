333806417c1b30858a8bdf4216c7e223af866ff1
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-04-23 16:37:09 +0530

    start create back end

---
+++ src/test/java/com/codemy/backdev/BackdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemy.backdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
